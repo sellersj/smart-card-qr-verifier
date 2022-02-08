a614ab1087560c7d7fe409edd7a7d0a3c817d6d4
Jim Sellers <jim.sellers@gmail.com>
2022-02-08 09:41:48 -0500

    adding a little spike for checking json rules

--- src/test/java/com/github/sellers/qrcodeverifier/QRCodeScannerSpikeTest.java
+++ src/test/java/com/github/sellers/qrcodeverifier/QRCodeScannerSpikeTest.java
@@ -5,16 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+
+import io.github.jamsesso.jsonlogic.JsonLogic;
 
 public class QRCodeScannerSpikeTest {
 
@@ -51,5 +56,21 @@ public class QRCodeScannerSpikeTest {
             assertNotNull(entry.getKey(), "key shouldbn't be null");
             assertNotNull(entry.getValue(), "value shouldbn't be null");
         }
+    }
+
+    // TODO see if we need this. Test for checking io.github.jamsesso:json-logic-java
+    @Test
+    @Disabled("a check of some outputs that are temp")
+    public void validateRules() throws Exception {
+        String dir = "/Users/sellersj/Documents/workspace/Temp/src/main/resources/";
+        String data = FileUtils.readFileToString(new File(dir + "temp3.json"), StandardCharsets.ISO_8859_1);
+        String rules = FileUtils.readFileToString(new File(dir + "temp2.json"), StandardCharsets.ISO_8859_1);
+
+        JsonLogic jsonLogic = new JsonLogic();
+
+        // Evaluate the result.
+        double result;
+        result = (double) jsonLogic.apply(rules, data);
+        assert result == 20.0;
     }
 }
